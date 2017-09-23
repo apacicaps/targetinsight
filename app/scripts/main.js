6073075f1cea6a3b6a142e7db37dd83a7b4ebf5a
apacicaps <saraegtved@gmail.com>
2017-09-23 16:44:24 +0200

    Content blocks

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('\'Allo \'Allo!');
+
+$('#intromodallink').on('click', function(){
+    $('#introModal').modal('show');
+})
+
+$('#aboutmodallink').on('click', function(){
+    $('#aboutModal').modal('show');
+})
+
+$('#projektledelselink').on('click', function(){
+    $('#projektledelseModal').modal('show');
+})
+
+$('#changemanagementlink').on('click', function(){
+    $('#changemanagementModal').modal('show');
+})
+
+$('#bestyrelsesarbejdelink').on('click', function(){
+    $('#bestyrelsesarbejdeModal').modal('show');
+})
