f89b6207bda950abbc1bd3232d779571a59b3b64
apacicaps <saraegtved@gmail.com>
2017-11-19 14:24:09 +0100

    Final

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,39 @@
+// Select all links with hashes
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top - 70
+      }, 600, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(':focus')) { // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          $target.focus(); // Set focus again
+        };
+      });
+    }
+  }
+});
 
 $('#intromodallink').on('click', function () {
     $('#introModal').modal('show');
@@ -45,6 +81,7 @@ function initSwiper(){
             slidesPerView: slidesPerView,
             slidesPerGroup: 1,
             loop: true,
+            initialSlide: 0,
             loopFillGroupWithBlank: true,
             pagination: {
                 el: '.swiper-pagination',
